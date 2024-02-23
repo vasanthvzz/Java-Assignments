c4c9d1a7a6eb15503c6bfbc9d10ba8f19bdd9771
Vasanth <vasanthrv84@gmail.com>
2024-02-23 14:33:08 +0530

    Changed FinalIndex.java

--- Assignment4/FindIndex.java
+++ Assignment4/FindIndex.java
@@ -1,17 +1,23 @@
 public class FindIndex{
 
     public static void main(String[] args) {
-        int[] arr = {12,54,23,56,211,1,4,65,40};
-        int n = 65;
+        int[] arr = {12,54,65,23,56,211,1,4,65,40};
+        int n = 12;
         System.out.println(indexFind(arr, n));
     }
 
     static int indexFind(int[] arr , int target){
+        boolean visited = false;
         for(int i = 0 ;i<arr.length;i++){
             if(arr[i] == target){
-                return i;
+                if(visited){
+                    return i;
+                }else{
+                    visited = true;
+                }
+
+            } 
             }
-        }
         return -1;
     }
 }
